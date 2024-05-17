d24fbd1d69750eb20eedb594c59fe9f4770ead37
Hannah Kamau <hannahkamau208@gmail.com>
2024-05-17 13:41:25 +0300

    Added download link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,42 @@
+import { Console } from "console";
 import Link from "next/link";
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  async function getDownloadPath()  {
+      return await (await fetch('http://localhost:3000/api/khis'))
+  }
+
+  const downloadPath = await getDownloadPath()
+
+  console.log({downloadPath})
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+          <span className="text-[hsl(280,100%,70%)]">KHIS</span> Analytics App
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
           <Link
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
+            href={`file:///`}
             target="_blank"
           >
             <h3 className="text-2xl font-bold">First Steps →</h3>
             <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
+              Download KHIS Analytics file
+              
             </div>
           </Link>
           <Link
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
+            href="#"
             target="_blank"
           >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
+            <h3 className="text-2xl font-bold">link →</h3>
             <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
+              {''}
             </div>
           </Link>
         </div>
--- src/env.js
+++ src/env.js
@@ -22,8 +22,12 @@ export const env = createEnv({
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
       process.env.VERCEL ? z.string() : z.string().url()
     ),
-    DISCORD_CLIENT_ID: z.string(),
-    DISCORD_CLIENT_SECRET: z.string(),
+    KHIS_API_URL: z.string(),
+    KHIS_API_USERNAME: z.string(),
+    KHIS_API_PASSWORD: z.string()
+
+    // DISCORD_CLIENT_ID: z.string(),
+    // DISCORD_CLIENT_SECRET: z.string(),
   },
 
   /**
@@ -44,8 +48,12 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+    KHIS_API_URL: process.env.KHIS_API_URL,
+    KHIS_API_USERNAME: process.env.KHIS_API_USERNAME,
+    KHIS_API_PASSWORD: process.env.KHIS_API_PASSWORD
+    // DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
+    // DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+    
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
